028a68995d15b8f58d5a7e374a67029598dbe7a7
Łukasz Sieczkowski <sieczkowski.l@gmail.com>
2018-09-29 14:16:52 +0200

    Initial commit

---
+++ src/main/java/com/codedito/HseApplication.java
@@ -0,0 +1,12 @@
+package com.codedito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HseApplication.class, args);
+	}
+}
