c74f2645542ab908a59e743372f9e3279107805e
Łukasz Sieczkowski <sieczkowski.l@gmail.com>
2018-10-08 18:51:45 +0200

    CommandRunner fix

--- userservice/src/main/java/com/codedito/userservice/UserServiceApplication.java
+++ userservice/src/main/java/com/codedito/userservice/UserServiceApplication.java
@@ -1,13 +1,21 @@
 package com.codedito.userservice;
 
+import com.codedito.userservice.model.Role;
+import com.codedito.userservice.model.User;
+import com.codedito.userservice.model.UserRole;
 import com.codedito.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class UserServiceApplication implements CommandLineRunner {
     @Autowired
     private UserService userService;
@@ -18,19 +26,19 @@ public class UserServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(final String... args) throws Exception {
-//        final User user1 = new User();
-//        user1.setFirstName("John");
-//        user1.setLastName("Adams");
-//        user1.setUsername("jadams");
-//        user1.setPassword("password");
-//        user1.setEmail("jadams@gmail.com");
-//
-//        final Set<UserRole> userRoles = new HashSet<>();
-////        final Role role1 = new Role();
-//        role1.setRoleId(1);
-//        role1.setName("ROLE_USER");
-//        userRoles.add(new UserRole(user1, role1));
-//
-//        userService.createUser(user1);
+        final User user1 = new User();
+        user1.setFirstName("John");
+        user1.setLastName("Adams");
+        user1.setUsername("jadams");
+        user1.setPassword("password");
+        user1.setEmail("jadams@gmail.com");
+
+        final Set<UserRole> userRoles = new HashSet<>();
+        final Role role1 = new Role();
+        role1.setRoleId(1);
+        role1.setName("ROLE_USER");
+        userRoles.add(new UserRole(user1, role1));
+
+        userService.createUser(user1);
     }
 }
