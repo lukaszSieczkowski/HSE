3a2c0b239d6e79f68d1d11f6e292ec1e06cbfbea
Łukasz Sieczkowski <sieczkowski.l@gmail.com>
2018-10-09 21:37:39 +0200

    ConfigServer

---
+++ configserver/src/main/java/com/codedito/configserver/configserver/ConfigserverApplication.java
@@ -0,0 +1,14 @@
+package com.codedito.configserver.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserverApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ConfigserverApplication.class, args);
+    }
+}
