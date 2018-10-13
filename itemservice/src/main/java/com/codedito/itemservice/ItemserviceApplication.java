f05a411e527aba24e084c02bd6c1c7246e543f74
Łukasz Sieczkowski <sieczkowski.l@gmail.com>
2018-10-13 18:55:47 +0200

    Itemservice registration

--- itemservice/src/main/java/com/codedito/itemservice/ItemserviceApplication.java
+++ itemservice/src/main/java/com/codedito/itemservice/ItemserviceApplication.java
@@ -7,13 +7,13 @@ import com.codedito.itemservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.Date;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableEurekaClient
 public class ItemserviceApplication implements CommandLineRunner {
 
     @Autowired
